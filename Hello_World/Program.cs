d8ca7d542419ec13b827d9370c3fadf6fc8b6097
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-04-15 19:53:53 +0200

    Premier Hello World

---
+++ Hello_World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world !!!");
+        }
+    }
+}
